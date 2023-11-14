f2ee74d419562315ab33d3dfb16b6fb4d443442f
zimboflyman <6759437+zimboflyman@users.noreply.github.com>
2023-11-14 13:37:35 +0000

    basic table styling

--- components/InvoiceTable.jsx
+++ components/InvoiceTable.jsx
@@ -1,38 +1,52 @@
 export default function InvoiceTable({ headers, invoiceData }) {
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            {headers.map((header, index) => (
-              <th key={index}>
-                <p>{header}</p>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {invoiceData.map((row) => {
-            return (
-              <tr key={row.Id}>
-                {Object.values(headers).map((header, index) => {
-                  let temp = row[header];
-                  return (
-                    <td key={index}>
-                      <p>{row[header]}</p>
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-          {!invoiceData.length && (
+    <section className="container mx-auto my-3">
+      <div>
+        <table className="min-w-full divide-y divide-gray-200 divide-gray-700">
+          <thead className="bg-gray-50 dark:bg-gray-800">
             <tr>
-              <td colSpan={4}>No invoices....</td>
+              {headers.map((header, index) => (
+                <th
+                  key={index}
+                  scope="col"
+                  className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-400"
+                >
+                  <p className="flex items-center gap-x-3">{header}</p>
+                </th>
+              ))}
             </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-700 bg-gray-900">
+            {invoiceData.map((row) => {
+              return (
+                <tr key={row.Id}>
+                  {Object.values(headers).map((header, index) => {
+                    let val = row[header];
+                    return (
+                      <td
+                        key={index}
+                        // conditionally align text right if its a number
+                        className={`px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap ${
+                          typeof val === "number" ? "text-right" : ""
+                        }`}
+                      >
+                        <p className="inline-flex items-center gap-x-3">
+                          {row[header]}
+                        </p>
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+            {!invoiceData.length && (
+              <tr>
+                <td colSpan={4}>No invoices....</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 }
